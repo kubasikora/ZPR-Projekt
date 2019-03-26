29112ea0da0826879fcdf49fb6a7cd74b4016557
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2019-03-26 17:01:21 +0100

    added sql script that adds devices

---
+++ db/insertDevices.sql
@@ -0,0 +1,4 @@
+insert into devices values (default, 'b31edaf2-4fb7-11e9-8647-d663bd873d93', 'CS7', 'Introl', '°C');
+insert into devices values (default, '99e6c2cb-68a3-4f27-a289-5be85ce96669', 'FLOMID XT5/XT5H', 'AEA-Technique', 'l/h');
+insert into devices values (default, 'd2fd535c-7ebd-4a29-b59e-702b9cb9617f', 'ADZ-SML-10.0', 'Wobit', 'bar');
+insert into devices values (default, 'd5397e20-43f9-457a-b582-3e9a64d56fed', 'AM2302', 'Aosong', '%');
