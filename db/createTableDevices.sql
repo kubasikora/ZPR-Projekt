8866c44c8d5a65f3240bddb6a860b174735ac332
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2019-03-26 17:16:36 +0100

    changed ddl scripts to collect information about device limits

--- db/createTableDevices.sql
+++ db/createTableDevices.sql
@@ -4,9 +4,11 @@ CREATE TABLE public.devices
     serial_code character varying NOT NULL,
     model character varying NOT NULL,
     producer character varying NOT NULL,
+    unit character varying NOT NULL,
+    min_value double precision NOT NULL,
+    max_value double precision NOT NULL,
     CONSTRAINT id PRIMARY KEY (device_id),
     CONSTRAINT unique_serial UNIQUE (serial_code)
-
 )
 WITH (
     OIDS = FALSE
