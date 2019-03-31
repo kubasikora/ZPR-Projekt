1f98d6f17929d421c3bba1188fa6f80205747e05
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2019-03-31 19:00:51 +0200

    made dbcontroller abstract with virtual destructor

--- scada/include/controllers/DatabaseController.hpp
+++ scada/include/controllers/DatabaseController.hpp
@@ -9,7 +9,8 @@ namespace zpr {
 class DatabaseController {
   public:
     DatabaseController(boost::python::dict& env);
-    
+    virtual ~DatabaseController() = 0;
+
     std::string host;
     std::string user;
     std::string password;
