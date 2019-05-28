b35c98b541ef68b8cacb3b3b85610a5068b1d03d
Julia <julia.klos159@gmail.com>
2019-05-29 01:07:24 +0200

    getData start

--- client/src/actions/getData.js
+++ client/src/actions/getData.js
@@ -1,8 +1,24 @@
 import {GET_DATA_START, GET_DATA_OK, GET_DATA_ERROR} from "../actionTypes"
 import axios from 'axios';
- function getData(state) {
-    return { type: GET_DATA_START
-    }
+ const getData =(state) =>{
+   return async dispatch =>{
+    try{
+      dispatch({type: GET_DATA_START});
+      let params = {
+        'startTime': state.startTime,
+        'stopTime': state.stopTime,
+        'deviceId': state.deviceId
+      }
+      let response = await(()=>axios.get("localhost:5000/getSerializedData", {params}));
+      console.log(response)
+      dispatch({type: GET_DATA_OK})
+    
+  }catch(err){
+    console.log(err)
+    dispatch({type: GET_DATA_ERROR})
   }
+ }
+}
+
 
 export default getData;
